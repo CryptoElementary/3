693b92c9e964596bc0ac1acb0a45ee8de090ffd6
1 <1>
2023-04-13 18:34:51 +0700

    Initial Commit

---
+++ Shared/_App.swift
@@ -0,0 +1,17 @@
+//
+//  _App.swift
+//  Shared
+//
+//  Created by AnKl on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct _App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
